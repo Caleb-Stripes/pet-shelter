960ef7ceb335a83a2ec0aaec89cdba8e930d48c7
Caleb-Stripes <calebdstripes@gmail.com>
2020-01-19 14:11:22 -0500

    added README.md file

---
+++ src/test/java/org/wecancodeit/petShelterTest.java
@@ -0,0 +1,14 @@
+package org.wecancodeit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class petShelterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
