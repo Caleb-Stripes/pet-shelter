7324abcc96997517db91f92a879cc491e7bb5dee
Caleb-Stripes <calebdstripes@gmail.com>
2020-01-19 15:33:10 -0500

    Imported virtual pet code and test code from other project and updated README.md

--- src/main/java/org/wecancodeit/VirtualPet.java
+++ src/main/java/org/wecancodeit/VirtualPet.java
@@ -1,5 +1,152 @@
 package org.wecancodeit;
 
 public class VirtualPet {
+	private int hunger = 0;
+	private int thirst = 0;
+	private int boredom = 0;
+	String status = "Alive";
+	String petName;
+
+	public VirtualPet(int hunger, int thirst, int boredom, String petName, String status) {
+		this.hunger = hunger;
+		this.thirst = thirst;
+		this.boredom = boredom;
+		this.status = status;
+		this.petName = petName;
+	}
+
+	public void shelterPet() {
+		this.hunger = 5;
+		this.thirst = 5;
+		this.boredom = 5;
+	}
+
+	public String getPetName() {
+
+		return this.petName;
+	}
+
+	public int getHunger() {
+
+		return this.hunger;
+	}
+
+	public int getThirst() {
+
+		return this.thirst;
+	}
+
+	public int getBoredom() {
+
+		return this.boredom;
+	}
+
+	public String getStatus() {
+		if (this.hunger >= 10 || this.thirst >= 10) {
+			this.status = "Dead";
+		}
+		return this.status;
+	}
+
+	public int eat() {
+		this.hunger = this.hunger - 3;
+		if (this.hunger < 0) {
+			this.hunger = 0;
+		}
+		return this.hunger;
+		// this.hunger--;
+		// return this.hunger;
+	}
+
+	public int drink() {
+		this.thirst = this.thirst - 5;
+		if (this.thirst < 0) {
+			this.thirst = 0;
+		}
+		return this.thirst;
+
+	}
+
+	public int play() {
+		this.boredom = this.boredom - 2;
+		if (this.boredom < 0) {
+			this.boredom = 0;
+		}
+		return this.boredom;
+	}
+
+	String fetchThing = null;
+
+	public String fetch() {
+		int fetchRand = (int) (Math.random() * 10 + 1);
+		if (fetchRand <= 3) {
+			fetchThing = petName + " brings you a red ball.";
+		}
+		if (fetchRand == 4) {
+			fetchThing = petName + " drags over a large branch.";
+		}
+		if (fetchRand == 5) {
+			fetchThing = petName
+					+ " crouches in front of you and regurgitates some bright yellow bile with rubber bits in it.";
+		}
+		if (fetchRand == 6) {
+			fetchThing = petName
+					+ " stares at you in confusion, while the stick you threw sinks into a mud puddle, \nas if to say \"That was foolish, now you have to get muddy\".";
+		}
+		if (fetchRand == 7 || fetchRand == 8) {
+			fetchThing = petName + " fetches the thing you threw, but now it is coverd in slobber.";
+		}
+		if (fetchRand == 9) {
+			fetchThing = petName
+					+ " is gone for a very long time, so long you begin to worry. \nAfter searching for 20 minutes you find \n"
+					+ petName + " sleeping in its bed.";
+		}
+		if (fetchRand == 10) {
+			fetchThing = petName + " and you play a loving game of catch.";
+		}
+		return fetchThing;
+	}
+
+	public void tick() {
+		this.hunger++;
+		this.thirst++;
+		this.boredom++;
+
+	}
+
+	public static String hungerWarning;
+	public static String thirstWarning;
+	public static String boredomWarning;
+
+	public void currentHungerStats() {
+		if (this.hunger > 7) {
+			hungerWarning = petName + " is starving.";
+		} else {
+			hungerWarning = petName + " seems saciated.";
+		}
+	}
+
+	public void currentThirstStats() {
+		if (this.thirst > 7) {
+			thirstWarning = petName + " is dying of thirst.";
+		} else {
+			thirstWarning = petName + " seems hydrated.";
+		}
+	}
+
+	public void currentBoredomStats() {
+		if (this.boredom > 11) {
+			boredomWarning = petName + " looks quite board.";
+		} else {
+			boredomWarning = petName + " looks content.";
+		}
+	}
+
+	public void testPet() {
+		this.hunger = 1;
+		this.thirst = 1;
+		this.boredom = 1;
+
+	}
 
 }
---
+++ src/test/java/org/wecancodeit/petTest.java
@@ -0,0 +1,122 @@
+package org.wecancodeit;
+
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class petTest {
+
+	VirtualPet underTest = new VirtualPet(0, 0, 0, null, null);
+	//Moved the underTest object of the VirtualPet2 class out of the test methods.
+	//This action makes it globally accessible by all the methods in the test class.
+		@Test
+		public void hungerShouldBeZeroAtStart() {
+			int  result = underTest.getHunger();
+			assertEquals(0, result);
+		}
+
+		@Test
+		public void thirstShouldBeZeroAtStart() {
+			int result = underTest.getThirst();
+			assertEquals(0, result);
+		}
+		
+		@Test
+		public void boredomShouldBeZeroAtStart() {
+			int result = underTest.getBoredom();
+			assertEquals(0, result);
+		}
+		
+		@Test
+		public void hungerShouldDecreaseBy3AfterEat() {
+			underTest.shelterPet();
+			int beforeEat = underTest.getHunger();
+			underTest.eat();
+			int result = underTest.getHunger();
+			assertEquals(beforeEat-3, result);
+		}
+		
+		@Test
+		public void thirstShouldDecreaseBy5AfterDrink() {
+			underTest.shelterPet();
+			int beforeDrink = underTest.getThirst();
+			underTest.drink();
+			int result = underTest.getThirst();
+			assertEquals(beforeDrink-5, result);
+		}
+		
+		@Test
+		public void boredomShouldDecreaseBy2AfterPlay() {
+			underTest.shelterPet();
+			int beforePlay = underTest.getBoredom();
+			underTest.play();
+			int result = underTest.getBoredom();
+			assertEquals(beforePlay-2, result);
+			
+		}
+		
+		@Test
+		public void ifHungerGoesBelowZeroShouldBeZero() {
+			underTest.testPet();
+			int beforeHunger = underTest.getHunger();
+			underTest.eat();
+			int afterHunger = underTest.getHunger();
+			assertEquals(1, beforeHunger);
+			assertEquals(0, afterHunger);
+		}
+
+		@Test
+		public void tickShouldIncreaseAllBy1() {
+			int boredomBeforeTick = underTest.getBoredom();
+			int hungerBeforeTick = underTest.getHunger();
+			int thirstBeforeTick = underTest.getThirst();
+			underTest.tick();
+			int boredomResult = underTest.getBoredom();
+			int hungerResult = underTest.getHunger();
+			int thirstResult = underTest.getThirst();
+			assertEquals(boredomBeforeTick + 1, boredomResult);
+			assertEquals(hungerBeforeTick + 1, hungerResult);
+			assertEquals(thirstBeforeTick + 1, thirstResult);
+		}
+		
+		@Test
+		public void petStatusShouldBeDead() {
+			underTest.tick();
+			underTest.tick();
+			underTest.tick();
+			underTest.tick();
+			underTest.tick();
+			underTest.tick();
+			underTest.tick();
+			underTest.tick();
+			underTest.tick();
+			underTest.tick();
+			String result = underTest.getStatus();
+			assertEquals("Dead", result);
+		}
+		
+		@Test
+		public void shelterPetHungerShouldAllBe5() {
+			underTest.shelterPet();
+			int result = underTest.getHunger();
+			assertEquals(5, result);
+		}
+		
+		@Test
+		public void shelterPetThirstShouldAllBe5() {
+			underTest.shelterPet();
+			int result = underTest.getThirst();
+			assertEquals(5, result);
+		}
+		
+		@Test
+		public void shelterPetBoredomShouldAllBe5() {
+			underTest.shelterPet();
+			int result = underTest.getBoredom();
+			assertEquals(5, result);
+		}
+
+}
